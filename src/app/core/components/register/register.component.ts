07e768fe53e9fb6d1e2c0f1b36b23f159836ddd6
pleasebeavailable <ante_sucur@hotmail.com>
2020-02-21 15:41:58 +0100

    Create angular project; Implement Roles and SelectOption; Implement Register component;

---
+++ src/app/core/components/register/register.component.ts
@@ -0,0 +1,64 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../login/auth.service';
+import {Router} from '@angular/router';
+import {SelectOption} from '../../../shared/models/SelectOption';
+import {Roles} from '../../../shared/models/Roles';
+import {AppMethods} from '../../../shared/AppMethods';
+import {Location} from "@angular/common";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  registerForm: FormGroup;
+  roles: Array<SelectOption>;
+  submitted: false;
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router, private location: Location) {
+  }
+
+  ngOnInit(): void {
+    this.roles = [];
+    Object.keys(Roles).forEach(role => this.roles.push(new SelectOption(Roles[role], role)));
+
+    this.registerForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      // firstName: ['', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]],
+      // lastName: ['', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmpassword: ['',
+        [
+          Validators.required,
+          AppMethods.matchValues('password')
+        ]],
+      role: ''
+    });
+  }
+
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      this.authService.register(this.registerForm.value).pipe().subscribe();
+    }
+  }
+
+  refreshRoleValue(value: any) {
+    this.registerForm.controls.role.patchValue(value.value);
+  }
+
+  onRoleClear() {
+    this.registerForm.controls.role.patchValue('');
+  }
+
+  goBack() {
+    this.location.back();
+  }
+}
